e53ec40ef27ea6ab04822b936e3eb04f9968a58d
Guillermo Rangel <memonew15@hotmail.com>
2018-02-20 15:56:39 -0600

    Initial Commit

---
+++ juegoDeMemoriaTests/juegoDeMemoriaTests.swift
@@ -0,0 +1,36 @@
+//
+//  juegoDeMemoriaTests.swift
+//  juegoDeMemoriaTests
+//
+//  Created by Guillermo Rangel on 20/02/18.
+//  Copyright © 2018 PicMemories. All rights reserved.
+//
+
+import XCTest
+@testable import juegoDeMemoria
+
+class juegoDeMemoriaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
